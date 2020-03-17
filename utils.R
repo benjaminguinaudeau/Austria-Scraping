dfe6bfce06c2a4e4dbb8a89cfa1a038f61d038b9
benjaminguinaudeau <bguinaudeau@gmail.com>
2020-03-17 16:12:32 -0400

    some fixes

--- utils.R
+++ utils.R
@@ -10,12 +10,6 @@ scrape_indexpage <- function(url) { # URL = XXV, XXIV, XXIII, ...
   #read in page
   indexpage <- read_html(url)
   
-  proposalpages <- indexpage %>%
-    html_nodes("a.link-indicator") %>%
-    html_attr("href") %>%
-    .[seq(1, length(.), by = 2)] %>%
-    paste0("https://www.parlament.gv.at", .)
-  
   # get dates of proposals (website upload?), type (RV, A, VOLKBG, BRA), descriptive title, 
   indexpagedata <- indexpage %>%
     html_nodes("span.table-responsive__inner") %>%
@@ -26,7 +20,11 @@ scrape_indexpage <- function(url) { # URL = XXV, XXIV, XXIII, ...
     setNames(c("web_date", "type", "desc_title", "proposal_id")) 
   
   indexpagedata <- indexpagedata %>%
-    mutate(proposal_link = proposalpages, 
+    mutate(proposal_link = indexpage %>%
+             html_nodes("a.link-indicator") %>%
+             html_attr("href") %>%
+             .[seq(1, length(.), by = 2)] %>%
+             paste0("https://www.parlament.gv.at", .), 
            period = url %>%
              str_extract("GP=\\w+") %>%
              str_sub(., start = 4), 
@@ -86,13 +84,14 @@ get_meta_info <- function(.x){
     .x$type %in% c("BUA") ~ {page %>%
         html_nodes("ul.fliesstext li a") %>%
         html_attr("href") %>%
-        str_subset("\\d.html") %>% .[2]}
+        str_subset("\\d.html") %>%
+        .[2]}
   ) %>%
     fix_obj
   
   iniator <- page %>%
     html_nodes("div.c_2 p") %>%
-    .[2] %>%
+    .[-1] %>%
     html_text(trim = T) %>%
     str_squish %>%
     str_extract("(?<=\\:)\\s*[A-Z].+") %>%
@@ -100,23 +99,32 @@ get_meta_info <- function(.x){
     str_subset("[0-9]{3}", negate = T) %>%
     str_subset("\\s{3,}", negate = T) %>%
     fix_obj
+    # fix_obj(pattern = list())
   
-  dates <- page %>%
-    html_nodes("table.table-nonresponsive") %>%
-    html_nodes("tr.historyShowAlways") %>%
+  verfahren <- page %>%
+    html_node(".tabelleHistorieResponsive") %>%
+    html_nodes(".historyShowAlways") %>%
     html_text(trim = T) %>%
-    str_extract("\\d{2}.\\d{2}.\\d{4}") %>%
-    discard(is.na) 
+    keep(~str_detect(.x, "\\d{2}.\\d{2}.\\d{4}")) %>%
+    tibble(verfahren = .) %>%
+    list
   
-  processes <- page %>%
-    html_nodes("a.historieOverviewToggle") %>%
-    html_text()
+  # dates <- page %>%
+  #   html_nodes("table.table-nonresponsive") %>%
+  #   html_nodes("tr.historyShowAlways") %>%
+  #   html_text(trim = T) %>%
+  #   str_extract("\\d{2}.\\d{2}.\\d{4}") %>%
+  #   discard(is.na) 
+  # 
+  # processes <- page %>%
+  #   html_nodes("a.historieOverviewToggle") %>%
+  #   html_text()
+  # 
+  # parl_verfahren <- tibble(date = dates,
+  #                          process = processes) %>%
+  #   list %>%
+  #   fix_obj(pattern = list())
   
-  parl_verfahren <- tibble(date = dates,
-                           process = processes) %>%
-    list %>%
-    fix_obj(pattern = list())
-  
-  return(tibble( .id = .x$.id, bill_link, bill_id, proposal_download, iniator, resolution_NR, parl_verfahren))
+  return(tibble::tibble( .id = .x$.id, bill_link, bill_id, proposal_link = .x$proposal_link, proposal_download, iniator, resolution_NR, verfahren))
   
 }
